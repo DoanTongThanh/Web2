6d9e12412179767dd8d8d61439df5bf4767fd157
doant <doant@MSI>
2026-09-23 17:47:02 +0700

    phan 2

---
+++ Web2/Models/DTO/BookDTO.cs
@@ -0,0 +1,18 @@
+﻿using Web2.Models.Domain;
+namespace Web2.Models.DTO
+{
+    public class BookDTO
+    {
+        public int Id { get; set; } 
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public bool IsRead { get; set; }
+        public DateTime? DateRead { get; set; }
+        public int? Rate { get; set; }
+        public string Genre { get; set; } = string.Empty;
+        public string CoverUrl { get; set; } = string.Empty;
+        public DateTime DateAdded { get; set; }
+        public string PublisherName { get; set; } = string.Empty;
+        public List<string>? AuthorNames { get; set; }
+    }
+}
---
+++ Web2/Models/DTO/addBookRequestDTO.cs
@@ -0,0 +1,17 @@
+﻿using Web2.Models.Domain;
+namespace Web2.Models.DTO
+{
+    public class addBookRequestDTO
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public bool IsRead { get; set; }
+        public DateTime? DateRead { get; set; }
+        public int? Rate { get; set; }
+        public string Genre { get; set; } = string.Empty;
+        public string CoverUrl { get; set; } = string.Empty;
+        public DateTime DateAdded { get; set; }
+        public int PublisherId { get; set; }
+        public List<int> AuthorIds { get; set; } = new List<int>();
+    }
+}
